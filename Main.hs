cccc266e55e899c063293769362a1415cf9fa59f
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-05-09 16:29:23 +0530

    Refactor

--- Main.hs
+++ Main.hs
@@ -1,79 +1,84 @@
-import Data.Char
-import Data.List
+module Main where
 
-data JsonValue
-  = JsonNull
-  | JsonBool Bool
-  | JsonString String
-  | JsonNumber Double
-  | Ok
+import Data.Char
+
+data JSON
+  = NULL
+  | BOOLEAN Bool
+  | STRING String
+  | ARRAY [JSON]
+  | NUMBER Double
+  | OK
   deriving (Show)
 
-type Parser = String -> Maybe (JsonValue, String)
+type Parser = String -> Maybe (JSON, String)
 
-nullParser, boolParser, spaceParser, colonParser, commaParser, numberParser, stringParser ::
+nullParser, boolParser, colonParser, commaParser, numberParser, stringParser ::
      Parser
 nullParser input
-  | take 4 input == "null" = Just ((JsonNull, drop 4 input))
+  | take 4 input == "null" = Just (NULL, drop 4 input)
   | otherwise = Nothing
 
 boolParser input
-  | take 4 input == "true" = Just (JsonBool True, drop 4 input)
-  | take 5 input == "false" = Just (JsonBool False, drop 5 input)
+  | take 4 input == "true" = Just (BOOLEAN True, drop 4 input)
+  | take 5 input == "false" = Just (BOOLEAN False, drop 5 input)
   | otherwise = Nothing
 
-spaceParser (x:rem)
-  | isSpace x = spaceParser rem
-spaceParser input = Just (Ok, input)
+removeSpace :: String -> String
+removeSpace (x:xs)
+  | isSpace x = removeSpace xs
+removeSpace input = input
 
-colonParser (x:rem)
-  | x == ':' = Just (Ok, rem)
+colonParser (x:xs)
+  | x == ':' = Just (OK, xs)
 colonParser _ = Nothing
 
-commaParser (x:rem)
-  | x == ',' = Just (Ok, rem)
+commaParser (x:xs)
+  | x == ',' = Just (OK, xs)
 commaParser _ = Nothing
 
 numberParser input =
-  case (reads input) :: [(Double, String)] of
-    [(num, rem)] -> Just (JsonNumber num, rem)
+  case reads input :: [(Double, String)] of
+    [(num, remaing)] -> Just (NUMBER num, remaing)
     _ -> Nothing
 
+validEscapeChars :: [Char]
 validEscapeChars = ['\"', '\\', '\b', '\f', '\n', '\r', '\t']
 
 -- ! TODO: handle unicode
 stringParser "" = Nothing
-stringParser ('"':'"':xs) = Just (JsonString "", xs)
+stringParser ('"':'"':xs) = Just (STRING "", xs)
 stringParser ('"':x:xs) =
   case x of
     '\\'
-      | (xs !! 0) `elem` validEscapeChars ->
+      | head xs `elem` validEscapeChars ->
         case stringParser ("\"" ++ drop 1 xs) of
-          Just (JsonString str, rem) ->
-            Just (JsonString (x : (xs !! 0) : str), rem)
+          Just (STRING str, remaing) ->
+            Just (STRING (x : head xs : str), remaing)
           _ -> Nothing
       | otherwise -> Nothing
     _ ->
       case stringParser ("\"" ++ xs) of
-        Just (JsonString str, rem) -> Just (JsonString (x : str), rem)
+        Just (STRING str, remaing) -> Just (STRING (x : str), remaing)
         _ -> Nothing
+stringParser _ = Nothing
 
 parsers :: [Parser]
 parsers = [nullParser, boolParser, numberParser, stringParser]
 
-valueParser :: [Parser] -> String -> Maybe (JsonValue, String)
-valueParser [] input = Nothing
-valueParser (p:parsers) input =
-  let res = (p input)
+valueParser :: [Parser] -> String -> Maybe (JSON, String)
+valueParser [] _ = Nothing
+valueParser (p:ps) input =
+  let res = p input
    in case res of
-        Nothing -> (valueParser parsers input)
+        Nothing -> valueParser ps input
         _ -> res
 
-parser :: String -> [JsonValue]
+parser :: String -> JSON
 parser input =
-  case (valueParser parsers input) of
-    Just (val, rem) -> val : parser rem
-    Nothing -> []
+  case valueParser parsers input of
+    Just (val, "") -> val
+    _ -> error "could not parse"
 
 main :: IO ()
 main = undefined
